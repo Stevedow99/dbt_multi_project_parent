7a1c3d53d786ce24ecf6026ae8ee49ae213f8622
Steve Dowling <steve.dowling@fishtownanalytics.com>
2024-12-04 00:56:47 +0000

    updates

---
+++ models/parent_project_models/filtered_order_summary.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        materialized = 'table',
+        tags=['finance', 'filtered']
+    )
+}}
+
+with base_data as (
+    select 
+        order_key, 
+        priority_code, 
+        net_item_sales_amount
+    from {{ ref('fct_orders') }}
+)
+
+select 
+    order_key,
+    priority_code,
+    net_item_sales_amount
+from base_data
+order by order_key
